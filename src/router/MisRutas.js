c0ac0cdccee3fabab78e7bccac4dffe023e1730b
nicolasvaldelamar <nicolasvaldelamar.c@gmail.com>
2023-04-09 23:04:57 -0500

    agregando estilos a el componente Portafolio

--- src/router/MisRutas.js
+++ src/router/MisRutas.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Routes, Route, BrowserRouter, Navigate} from 'react-router-dom'
+import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom'
 import { Inicio } from '../components/Inicio'
 import { Portafolio } from '../components/Portafolio'
 import { Servicio } from '../components/Servicio'
@@ -11,27 +11,34 @@ import { Footer } from '../components/layout/Footer'
 export const MisRutas = () => {
   return (
     <div>
-        <BrowserRouter>
-            {/*HEADER Y NAVEGACION*/}
-            <HeaderNav/>
-            {/**CONTENIDO CENTRAL */}
-            <section className='content'>
-            <Routes>
-                <Route path="/" element={<Navigate to='/inicio'/>}/>
-                <Route path="/inicio" element={<Inicio/>}/>
-                <Route path="/portafolio" element={<Portafolio/>}/>
-                <Route path="/servicios" element={<Servicio/>}/>
-                <Route path="/curriculum" element={<Curriculum/>}/>
-                <Route path="/contacto" element={<Contacto/>}/>
+      <BrowserRouter>
+        {/*HEADER Y NAVEGACION*/}
+        <HeaderNav />
+        {/**CONTENIDO CENTRAL */}
+        <section className='content'>
+          <Routes>
+            <Route path="/" element={<Navigate to='/inicio' />} />
+            <Route path="/inicio" element={<Inicio />} />
+            <Route path="/portafolio" element={<Portafolio />} />
+            <Route path="/servicios" element={<Servicio />} />
+            <Route path="/curriculum" element={<Curriculum />} />
+            <Route path="/contacto" element={<Contacto />} />
+            <Route path="*" element={
+              <div className='page'>
+                <h1 className='heading'>Error 404</h1>
 
-            </Routes>
-            </section>
+              </div>
+            } />
 
-          
-         
-            {/**FOOTER */}
-            <Footer/>
-        </BrowserRouter>
+
+          </Routes>
+        </section>
+
+
+
+        {/**FOOTER */}
+        <Footer />
+      </BrowserRouter>
     </div>
   )
 }

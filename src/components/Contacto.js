9a06b5a5517605cc445cb1091f979ecc448a216a
nicolasvaldelamar <nicolasvaldelamar.c@gmail.com>
2023-04-09 22:08:38 -0500

    agregando algunos estilos a los componentes principales

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -3,16 +3,13 @@ import React from 'react'
 export const Contacto = () => {
   return (
     <div className='page'>
-      <h1>Contacto</h1>
-
-      <form className='contact' action='mailto:nicolasvaldelamar.c@gmail.com'>
+      <h1 className='heading'>Contacto</h1>
+      <form className='contact' action='mailto:nicolasvaldealmar.c@gmail.com'>
         <input type='text' placeholder='Nombre'/>
         <input type='text' placeholder='Apellido'/>
         <input type='email' placeholder='Email'/>
         <textarea placeholder='Mensaje'></textarea>
         <input type='submit'  value='Enviar'/>
-
-
       </form>
     </div>
   )
--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -3,17 +3,17 @@ import { Link } from 'react-router-dom'
 
 export const Inicio = () => {
   return (
-    <div>
+    <div className='home'> 
       <h1>
-        Hola, soy Nicolás Valdelamar y soy Desarrollador Full Stack en Cartagena, 
-        y ofrezco mis servicios de programacion y desarrollo de software.
+        Hola, soy Nicolás Valdelamar y soy <strong>Desarrollador Full Stack</strong> en Cartagena, 
+        y ofrezco mis servicios de <strong>programacion</strong> y desarrollo de software.
       </h1>
       <h2>
           Te ayudo a crear tu solucion de software ya sea web,
           mobile o desktop y tener mayor productividad. <Link to='/contacto'>Contacta conmigo</Link>
       </h2>
       <section className='lasts-works'>
-        <h2>Algunos de mis proyectos</h2>
+        <h2 className='heading'>Algunos de mis proyectos</h2>
         <p>Estos son algunos de mis trabajos de desarrollo web.</p>
         <div className='works'>
 
